89acfc781d27f2aa459249fc540106dd5a5a4a55
shankey raheja <shankeyraheja@gmail.com>
2023-02-12 14:32:25 +1300

    tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import { FaBuffer } from "react-icons/fa"
 
 function App() {
+  const handleClick = () => {
+    let element = document.getElementById("navlinks")
+    const computedStyle = window.getComputedStyle(element, null)
+    console.log(computedStyle.display)
+
+    if (computedStyle.display === "none") {
+      element.style = "display:flex"
+    } else {
+      element.style = "display:none"
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">
+      <div className="logo">LOGO</div>
+      <div className="navlinks">
+        <ul id="navlinks">
+          <li>HOME</li>
+          <li>ABOUT</li>
+          <li>SHOP</li>
+          <li>CONTACT</li>
+        </ul>
+      </div>
+      <div className="burger" onClick={handleClick}>
+        <FaBuffer />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
